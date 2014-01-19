6e3224f13fdb10155adcce3472541dba5e29a8f9
Dan Garland <dan@dangarland.co.uk>
2014-01-19 12:57:59 +0000

    Using block form for builder

--- lib/funky-select/action_view/helpers/form_builder.rb
+++ lib/funky-select/action_view/helpers/form_builder.rb
@@ -1,11 +1,13 @@
 class ActionView::Helpers::FormBuilder
 
   def funky_select(method, options = {})
-    @template.content_tag(:div, "Hello there matey", :class => "funky-select #{method}-picker"
+    @template.content_tag :div, :class => "funky-select #{method}-picker" do
+
+    end
       # @template.radio_button(
       #   @object_name, method, tag_value, objectify_options(options)
       # )
-    )
+
   end
 
 end
