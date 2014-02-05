b6eed2d0383932df80b50ed3d85073d8fa758d47
Dan Garland <dan@dangarland.co.uk>
2014-02-05 10:01:22 +0000

    Adjusting order of JS dependencies

--- app/assets/javascripts/funky-toolset.js
+++ app/assets/javascripts/funky-toolset.js
@@ -1,2 +1,6 @@
-//= require 'funky-select'
+//= require lib/ejs_production
+//= require boot
+//= require_tree ./global
+//= require global/supa-uploader/supa-uploader
 //= require 'funky-uploader'
+//= require 'funky-select'
--- app/assets/javascripts/global/supa-uploader/supa-uploader.js
+++ app/assets/javascripts/global/supa-uploader/supa-uploader.js
@@ -3,4 +3,3 @@
 //= require_tree ./templates
 //= require_tree ./models
 //= require_tree ./views
-//= require_tree ./routers
