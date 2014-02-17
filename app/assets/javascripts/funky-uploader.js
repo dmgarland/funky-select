b9b9ff22e7575df19b051095ba104da02b7a540e
Dan Garland <dan@dangarland.co.uk>
2014-02-17 11:08:07 +0000

    Removing unneeded files

--- app/assets/javascripts/funky-uploader.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var image = new supaUploader.models.Image();
-    var view = new supaUploader.views.supaUploaderView({ model : image });
-    var uploader = view.render();
-    $("#supauploader-holder").html(uploader.el);
-});
